7fd619e19e387054ba985bb97ae733bd40117661
pengxianhong <290677893@qq.com>
2019-05-30 13:51:45 +0800

    添加一周内天气展示

--- app/src/main/java/com/pengxh/app/weatherplus/adapter/WeeklyRecyclerViewAdapter.java
+++ app/src/main/java/com/pengxh/app/weatherplus/adapter/WeeklyRecyclerViewAdapter.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import com.pengxh.app.weatherplus.R;
 import com.pengxh.app.weatherplus.bean.WeatherBean;
+import com.pengxh.app.weatherplus.utils.OtherUtil;
 
 import java.util.List;
 
@@ -46,24 +47,56 @@ public class WeeklyRecyclerViewAdapter extends RecyclerView.Adapter {
 
     class WeeklyRecyclerViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView mMVPRecyclerViewTime;
-        private TextView mMVPRecyclerViewWeather;
-        private TextView mMVPRecyclerViewTemp;
-        private ImageView mMVPRecyclerViewImg;
+        private TextView mTextView_weekly_date;
+        private TextView mTextView_weekly_week;
+
+        private ImageView mIncludeImageView_day_img;
+        private TextView mIncludeTextView_day_weather;
+        private TextView mIncludeTextView_day_temphigh;
+        private TextView mIncludeTextView_day_winddirect;
+        private TextView mIncludeTextView_day_windpower;
+
+        private ImageView mIncludeImageView_night_img;
+        private TextView mIncludeTextView_night_weather;
+        private TextView mIncludeTextView_night_templow;
+        private TextView mIncludeTextView_night_winddirect;
+        private TextView mIncludeTextView_night_windpower;
 
         private WeeklyRecyclerViewHolder(View itemView) {
             super(itemView);
-//            mMVPRecyclerViewTime = itemView.findViewById(R.id.mWeek);
-//            mMVPRecyclerViewWeather = itemView.findViewById(R.id.mMVP_RecyclerView_weather);
-//            mMVPRecyclerViewImg = itemView.findViewById(R.id.mMVP_RecyclerView_img);
-//            mMVPRecyclerViewTemp = itemView.findViewById(R.id.mMVP_RecyclerView_temp);
+            mTextView_weekly_date = itemView.findViewById(R.id.mTextView_weekly_date);
+            mTextView_weekly_week = itemView.findViewById(R.id.mTextView_weekly_week);
+            mIncludeImageView_day_img = itemView.findViewById(R.id.mIncludeImageView_day_img);
+            mIncludeTextView_day_weather = itemView.findViewById(R.id.mIncludeTextView_day_weather);
+            mIncludeTextView_day_temphigh = itemView.findViewById(R.id.mIncludeTextView_day_temphigh);
+            mIncludeTextView_day_winddirect = itemView.findViewById(R.id.mIncludeTextView_day_winddirect);
+            mIncludeTextView_day_windpower = itemView.findViewById(R.id.mIncludeTextView_day_windpower);
+            mIncludeImageView_night_img = itemView.findViewById(R.id.mIncludeImageView_night_img);
+            mIncludeTextView_night_weather = itemView.findViewById(R.id.mIncludeTextView_night_weather);
+            mIncludeTextView_night_templow = itemView.findViewById(R.id.mIncludeTextView_night_templow);
+            mIncludeTextView_night_winddirect = itemView.findViewById(R.id.mIncludeTextView_night_winddirect);
+            mIncludeTextView_night_windpower = itemView.findViewById(R.id.mIncludeTextView_night_windpower);
         }
 
         void bindHolder(WeatherBean.ResultBeanX.ResultBean.DailyBean dailyBean) {
-//            mMVPRecyclerViewTime.setText(hourlyBean.getTime());
-//            mMVPRecyclerViewWeather.setText(hourlyBean.getWeather());
-//            mMVPRecyclerViewImg.setImageResource(getImageResource(hourlyBean.getImg()));
-//            mMVPRecyclerViewTemp.setText(hourlyBean.getTemp() + "°");
+            mTextView_weekly_date.setText(dailyBean.getDate());
+            mTextView_weekly_week.setText(dailyBean.getWeek());
+            float sunrise = Float.parseFloat(dailyBean.getSunrise());
+            float sunset = Float.parseFloat(dailyBean.getSunset());
+
+            WeatherBean.ResultBeanX.ResultBean.DailyBean.DayBean dayBean = dailyBean.getDay();
+            mIncludeImageView_day_img.setImageResource(OtherUtil.getImageResource(mContext, dayBean.getImg()));
+            mIncludeTextView_day_weather.setText(dayBean.getWeather());
+            mIncludeTextView_day_temphigh.setText(dayBean.getTemphigh() + "℃");
+            mIncludeTextView_day_winddirect.setText(dayBean.getWinddirect());
+            mIncludeTextView_day_windpower.setText(dayBean.getWindpower());
+
+            WeatherBean.ResultBeanX.ResultBean.DailyBean.NightBean nightBean = dailyBean.getNight();
+            mIncludeImageView_night_img.setImageResource(OtherUtil.getImageResource(mContext, nightBean.getImg()));
+            mIncludeTextView_night_weather.setText(nightBean.getWeather());
+            mIncludeTextView_night_templow.setText(nightBean.getTemplow() + "℃");
+            mIncludeTextView_night_winddirect.setText(nightBean.getWinddirect());
+            mIncludeTextView_night_windpower.setText(nightBean.getWindpower());
         }
     }
 }
